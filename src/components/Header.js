c3565cb96efd2982223ab9f0f04db90dfb9b582c
Gildeir <gildeirlopes@gmail.com>
2021-06-14 18:13:17 -0300

    requisito 04 adding header

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import md5 from 'crypto-js/md5'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+render() {
+  const { emailDoUsuário, userName } = this.props;
+  const hash = md5(emailDoUsuário).toString();
+
+    return (
+      <header>
+      <div>
+        <img
+          src = {
+            `https://www.gravatar.com/avatar/${hash}`
+          }
+          alt="gravatar"
+          data-test="header-profile-picture"  
+        />
+      </div>
+    
+      <div>
+        <span data-testid="header-player-name"> {userName} </span>
+      </div>
+      <div>
+        <span data-testid="header-score"> 0 </span>
+      </div>
+      </header>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  emailDoUsuário: state.player.email,
+  userName: state.player.nome
+})
+
+Header.propTypes = {
+  emailDoUsuário: PropTypes.string,
+  userName: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Header);
